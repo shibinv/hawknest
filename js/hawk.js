9c8d11e8aa5de2d85bd364ec1af43fe30912191b
shibinv@gmail.com <shibinv@gmail.com>
2014-11-04 17:18:55 -0600

    Fixed a bug in LoadCourse();

--- js/hawk.js
+++ js/hawk.js
@@ -19,11 +19,38 @@ function LoadCourse() {
         $.each(data, function (key, val) {
             if (key === 'status'){
                 console.log(val);
-                //$("#course").empty().fadeOut(); // clear the dropdown on success
+                $("#course").empty(); //.fadeOut(); // clear the dropdown on success
             } else {
                 $.each(val, function(row, item) {
                     $("#course")
                         .append($('<option>', { row : item.course_number })
+                        .text(item.course_number));
+                    //$("#course").fadeIn();
+                    console.log(item.course_number);
+                });
+            }
+        });
+    });
+}
+
+function LoadSection() {
+    // selected value of department
+    value = $("#course").val();
+
+    console.log(value);
+    //console.log($.getJSON(url + '?request=course&value=' + value));
+    console.log(url + '?request=section&value=' + value);
+    
+    $.getJSON(url + '?request=section&value=' + value, function (data) {
+
+        $.each(data, function (key, val) {
+            if (key === 'status'){
+                console.log(val);
+                $("#course").empty(); //.fadeOut(); // clear the dropdown on success
+            } else {
+                $.each(val, function(row, item) {
+                    $("#course")
+                        .append($('<option>', { row : item.section })
                         .text(item.course_number));
                     //$("#course").fadeIn();
                     console.log(item.course_number);
