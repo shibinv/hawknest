e721ded8cf68b064a3f895b449d7cb88bc020353
shibinv@gmail.com <shibinv@gmail.com>
2014-11-30 16:02:01 -0600

    Added more faqs, link to logo, fixed map resize.

--- tutorial.php
+++ tutorial.php
@@ -9,13 +9,15 @@ require_once 'php/menu.php';
 
 <div align="center">
     
-<video width="400" controls>
-  <source src="misc/camtest.wmv" type="video/wmv">
+<video width="400" height="400" controls>
+  <source src="misc/camtest.mp4" type="video/mp4">
   Your browser does not support HTML5 video.
 </video>
 
-
-<embed src="misc/tutorial.pdf" width=800px height=2100px>
+    <div class="pure-g">
+        <div class="pure-u-1"><a href="misc/tutorial.pdf" target="_blank">View Tutorial PDF</a></div>
+    </div>
+    
 
 
 </div>
