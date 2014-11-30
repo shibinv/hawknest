45b1241f931c0235d1034f4e68dd09c45353923b
aboyslim <adnanmsahmad@gmail.com>
2014-11-30 11:34:18 -0600

    Added tutorial pdf and video source files and coded them in tutorial.php. Also added some questions on faq.php

--- faq.php
+++ faq.php
@@ -17,6 +17,20 @@ require_once 'php/menu.php';
                     Q. How much wood would a woodchuck chuck, if a woodchuck could chuck wood?
                     <br>
                     A. A woodchuck would chuck as much wood as a woodchuck could chuck, if a woodchuck could chuck wood.
+                    <br>
+                    Q. Is there only one entrance displayed on the map?
+                    <br>
+                    A. There are actually 2 entrances that can be used as exits as well. However, only one entrance/exit is used
+                       for demonstration purposes. 
+                    <br>
+                    Q. Why do the maps not show for all classes?
+                    <br>
+                    A. Some of the classes listed are online. Therefore, they do not require map to be displayed.
+                    <br>
+                    Q. What if a visitor needs to find a classroom for an event?
+                    <br>
+                    A. Non UHCL members can use the search option to locate the room they desire.
+                    
                 </div>
 
 
--- tutorial.php
+++ tutorial.php
@@ -5,7 +5,19 @@ require_once 'php/header.php';
 require_once 'php/menu.php';
 ?>
 
-                <div align="center">{Tutorial Video Here}</div>
+<!--                <div align="center">{Tutorial Video Here}</div>-->
 
+<div align="center">
+    
+<video width="400" controls>
+  <source src="camTest.wmv" type="video/wmv">
+  Your browser does not support HTML5 video.
+</video>
+
+
+<embed src="Hawknest tutorial.pdf" width=800px height=2100px>
+
+
+</div>
 
 <?php require_once 'php/footer.php';
