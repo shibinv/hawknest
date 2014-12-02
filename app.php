5244a08e0d473a01f39ae7dca46875335d0732c4
shibinv@gmail.com <shibinv@gmail.com>
2014-12-02 15:29:43 -0600

    Added Mobile Application and instructions.

--- app.php
+++ app.php
@@ -5,7 +5,14 @@ require_once 'php/header.php';
 require_once 'php/menu.php';
 ?>
 
-                <div align="center">{Mobile App Download}</div>
-
+                <div align="center" class="pure-g">
+                    
+                    <div class="pure-u-1">
+                        <a href="misc/hawkeyetest.apk">Download the Mobile Application</a>
+                    </div>
+                    <div class="pure-u-1">
+                        <a href="misc/apk-install-instructions.pdf" target="_blank">Install Instructions</a><br><br>
+                    </div>  
+                </div>
 
 <?php require_once 'php/footer.php';
